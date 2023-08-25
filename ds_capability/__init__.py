9d203010f51846f20ba3d6901757c13a004d76c5
gigas64 <gigas64@opengrass.net>
2023-08-25 10:39:17 -0500

    - fix get_synthetic_data_types

--- ds_capability/__init__.py
+++ ds_capability/__init__.py
@@ -4,4 +4,4 @@ from ds_capability.components.controller import Controller
 
 
 # release version number picked up in the setup.py
-__version__ = '0.6.7'
+__version__ = '0.7.0'
--- ds_capability/intent/feature_build_intent.py
+++ ds_capability/intent/feature_build_intent.py
@@ -839,7 +839,7 @@ class FeatureBuildIntentModel(FeatureBuildCorrelateIntent):
             if pa.types.is_dictionary(column.type):
                 selection = column.dictionary.to_pylist()
                 frequency = column.value_counts().field(1).to_pylist()
-                result = self.get_category(selection=selection, relative_freq=frequency, size=size, intent_level=c,
+                result = self.get_category(selection=selection, relative_freq=frequency, size=size, to_header=c,
                                            quantity=1-nulls, save_intent=False)
             elif pa.types.is_integer(column.type) or pa.types.is_floating(column.type):
                 s_values = column.to_pandas()
@@ -936,48 +936,48 @@ class FeatureBuildIntentModel(FeatureBuildCorrelateIntent):
         # cat
         canonical = self.get_category(selection=['SUSPENDED', 'ACTIVE', 'PENDING', 'INACTIVE', 'ARCHIVE'], canonical=canonical,
                               size=size, seed=seed, relative_freq=[1, 70, 20, 30, 10], encode=category_encode,
-                              intent_level='cat', save_intent=False)
+                              to_header='cat', save_intent=False)
         # num
-        canonical = self.get_dist_normal(mean=0, std=1, canonical=canonical, size=size, seed=seed, intent_level='num', save_intent=False)
-        canonical = self.correlate_number(canonical, 'num', precision=5, jitter=2, seed=seed, intent_level='num', save_intent=False)
+        canonical = self.get_dist_normal(mean=0, std=1, canonical=canonical, size=size, seed=seed, to_header='num', save_intent=False)
+        canonical = self.correlate_number(canonical, 'num', precision=5, jitter=2, seed=seed, to_header='num', save_intent=False)
         # int
-        canonical = self.get_number(start=size, stop=size * 10, at_most=1, canonical=canonical, size=size, seed=seed, intent_level='int',
+        canonical = self.get_number(start=size, stop=size * 10, at_most=1, canonical=canonical, size=size, seed=seed, to_header='int',
                             save_intent=False)
         # bool
-        canonical = self.get_boolean(size=size, probability=0.7, canonical=canonical, seed=seed, intent_level='bool', save_intent=False)
+        canonical = self.get_boolean(size=size, probability=0.7, canonical=canonical, seed=seed, to_header='bool', save_intent=False)
         # date
         canonical = self.get_datetime(start='2022-12-01', until='2023-03-31', ordered=True, canonical=canonical, size=size,
-                              seed=seed, intent_level='date', save_intent=False)
+                              seed=seed, to_header='date', save_intent=False)
         # string
-        canonical = self.get_sample(sample_name='us_street_names', canonical=canonical, size=size, seed=seed, intent_level='string',  save_intent=False)
+        canonical = self.get_sample(sample_name='us_street_names', canonical=canonical, size=size, seed=seed, to_header='string',  save_intent=False)
 
         if isinstance(inc_nulls, bool) and inc_nulls:
             gen = np.random.default_rng()
             prob_nulls = (gen.integers(1, 10, 1) * 0.001)[0] + prob_nulls
             # cat_null
             canonical = self.get_category(selection=['High', 'Med', 'Low'], canonical=canonical, relative_freq=[9,8,4], quantity=1 - prob_nulls,
-                                  intent_level='cat_null', size=size, encode=category_encode, seed=seed,
+                                  to_header='cat_null', size=size, encode=category_encode, seed=seed,
                                   save_intent=False)
             # num_null
             prob_nulls = (gen.integers(1, 10, 1) * 0.001)[0] + prob_nulls
             canonical = self.get_number(start=-1.0, stop=1.0, canonical=canonical, relative_freq=[1, 1, 2, 3, 5, 8, 13, 21], size=size,
-                                quantity=1 - prob_nulls, intent_level='num_null', seed=seed, save_intent=False)
+                                quantity=1 - prob_nulls, to_header='num_null', seed=seed, save_intent=False)
             # int_null
             prob_nulls = (gen.integers(1, 10, 1) * 0.001)[0] + prob_nulls
-            canonical = self.get_number(start=-1000, stop=1000, canonical=canonical, size=size, quantity=1 - prob_nulls, intent_level='int_null',
+            canonical = self.get_number(start=-1000, stop=1000, canonical=canonical, size=size, quantity=1 - prob_nulls, to_header='int_null',
                                 seed=seed, save_intent=False)
             # bool_null
             prob_nulls = (gen.integers(1, 10, 1) * 0.001)[0] + prob_nulls
             canonical = self.get_boolean(size=size, probability=0.4, canonical=canonical, seed=seed, quantity=1 - prob_nulls,
-                                 intent_level='bool_null', save_intent=False)
+                                 to_header='bool_null', save_intent=False)
             # date_null
             prob_nulls = (gen.integers(1, 10, 1) * 0.001)[0] + prob_nulls
             canonical = self.get_datetime(start='2022-12-01', until='2023-03-31', canonical=canonical, ordered=True, size=size, quantity=1 - prob_nulls,
-                                  intent_level='date_null', seed=seed, save_intent=False)
+                                  to_header='date_null', seed=seed, save_intent=False)
             # string_null
             prob_nulls = (gen.integers(1, 10, 1) * 0.001)[0] + prob_nulls
             canonical = self.get_sample(sample_name='us_cities', canonical=canonical, size=size, quantity=1 - prob_nulls,
-                                intent_level='string_null', seed=seed, save_intent=False)
+                                to_header='string_null', seed=seed, save_intent=False)
             # nulls_int
             _ = pa.table([pa.array(pa.nulls(size), pa.int64())], names=['nulls_int'])
             canonical = Commons.table_append(canonical, _)
@@ -992,7 +992,7 @@ class FeatureBuildIntentModel(FeatureBuildCorrelateIntent):
             canonical = Commons.table_append(canonical, _)
             # binary
             canonical = self.get_string_pattern(pattern='cccccccc', canonical=canonical, as_binary=True, size=size,
-                                                seed=seed, intent_level='binary', save_intent=False)
+                                                seed=seed, to_header='binary', save_intent=False)
             # list array
             _ = pa.array(list(zip(canonical.column('num').to_pylist(), canonical.column('num_null').to_pylist())))
             _ = pa.table([_], names=['nest_list'])
@@ -1043,7 +1043,7 @@ class FeatureBuildIntentModel(FeatureBuildCorrelateIntent):
             a = generator.choice(range(1, 6))
             b = generator.choice(range(1, 6))
             _ = self.get_distribution(distribution='beta', a=a, b=b, precision=6, size=size, seed=seed,
-                                      intent_level=f"{name_prefix}{next(label_gen)}", save_intent=False)
+                                      to_header=f"{name_prefix}{next(label_gen)}", save_intent=False)
             rtn_tbl = Commons.table_append(rtn_tbl, _)
         return Commons.table_append(canonical, rtn_tbl)
 
